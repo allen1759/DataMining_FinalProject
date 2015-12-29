fc0301ba0aea000917309362081826e955539fd1
Allen Yang <allen1759@gmail.com>
2015-12-29 17:53:59 +0800

    create 2 attribute files.

---
+++ DataMining/Attribute.cpp
@@ -0,0 +1,9 @@
+//
+//  Attribute.cpp
+//  DataMining
+//
+//  Created by 楊智偉 on 12/29/15.
+//  Copyright © 2015 楊智偉. All rights reserved.
+//
+
+#include "Attribute.h"
---
+++ DataMining/Attribute.h
@@ -0,0 +1,13 @@
+//
+//  Attribute.h
+//  DataMining
+//
+//  Created by 楊智偉 on 12/29/15.
+//  Copyright © 2015 楊智偉. All rights reserved.
+//
+
+#ifndef Attribute_h
+#define Attribute_h
+
+
+#endif /* Attribute_h */
