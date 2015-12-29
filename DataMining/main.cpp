2ddd45de871f0d24fdfc07176fcaedf515d049ed
Allen Yang <allen1759@gmail.com>
2015-12-29 17:44:08 +0800

    try to open text file.

--- DataMining/main.cpp
+++ DataMining/main.cpp
@@ -7,10 +7,21 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
+    
+    fstream in;
+    in.open("/Users/Allen/Documents/workspace/DataMining/expanded.txt", ios::in);
+    string line;
+    while( getline(in, line) ) {
+        cout << line << endl;
+    }
+    
     cout << "Hello, World!\n";
+    
     return 0;
 }
