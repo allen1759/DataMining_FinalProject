bff76333d9a463ded483cbfdb2ee859df34b1a76
Allen Yang <allen1759@gmail.com>
2015-12-29 16:04:16 +0800

    Initial Commit

---
+++ DataMining/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataMining
+//
+//  Created by 楊智偉 on 12/29/15.
+//  Copyright © 2015 楊智偉. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
