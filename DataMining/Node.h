361c963d68d0d2b904ec24ca28b35ecebb866941
Allen Yang <allen1759@gmail.com>
2015-12-31 11:33:49 +0800

    add some functions to record number of training data in a node.

--- DataMining/Node.h
+++ DataMining/Node.h
@@ -10,20 +10,66 @@
 #define Node_h
 
 #include <vector>
+#include "Data.h"
 
 class Node
 {
+private:
+    int cntCanEat = 0;
+    int cntCannotEat = 0;
+    
 public:
     bool isLeaf = false;
     bool hasValue = false;
     bool canEat;
     int question;
-    int cntCanEat = 0;
-    int cntCannotEat = 0;
     // 每一個分枝的值
     // std::vector<int> value;
     // 每一個分枝的 child node
     std::vector<Node *> child;
+    int getCntCanEat() const
+    {
+        return cntCanEat;
+    }
+    int getCntCannotEat() const
+    {
+        return cntCannotEat;
+    }
+    int getTotalCnt() const
+    {
+        return cntCanEat + cntCannotEat;
+    }
+    double getCanEatRate() const
+    {
+        return static_cast<double>(cntCanEat) / getTotalCnt();
+    }
+    double getCannotEateRate() const
+    {
+        return static_cast<double>(cntCannotEat) / getTotalCnt();
+    }
+    // case for non-hasValue node.
+    void updateCntEat()
+    {
+        cntCanEat = 0;
+        cntCannotEat = 0;
+        
+        for( auto & ptr : child ) {
+            cntCanEat += ptr->getCntCanEat();
+            cntCannotEat += ptr->getCntCannotEat();
+        }
+    }
+    // case for hasValue node.
+    void updateCntEat(std::vector<Data> & arr)
+    {
+        cntCanEat = 0;
+        cntCannotEat = 0;
+        for( auto &e : arr ) {
+            if( e.canEat )
+                cntCanEat += 1;
+            else
+                cntCannotEat += 1;
+        }
+    }
 };
 
 #endif /* Node_h */
